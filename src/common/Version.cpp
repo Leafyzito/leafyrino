9cbb57ab78d33ee7d611df2c12604e0ce80f166e
leafyzito <rafaelzao2003@gmail.com>
2026-02-12 18:56:05 -0100

    fix: update commit link in Version.cpp to point to the correct repository

--- src/common/Version.cpp
+++ src/common/Version.cpp
@@ -140,7 +140,7 @@ void Version::generateBuildString()
     // Add commit information
     s +=
         QString(
-            R"( (commit <a href="https://github.com/SevenTV/chatterino7/commit/%1">%1</a>)")
+            R"( (commit <a href="https://github.com/leafyzito/leafyrino/commit/%1">%1</a>)")
             .arg(this->commitHash());
     if (this->isModified())
     {
