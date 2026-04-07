03300351abde42344c75e1de3a28f9611f4d7125
leafyzito <rafaelzao2003@gmail.com>
2026-04-07 20:57:47 +0000

    fix: make Twitch (Device) the default login

--- src/widgets/dialogs/LoginDialog.cpp
+++ src/widgets/dialogs/LoginDialog.cpp
@@ -265,8 +265,11 @@ LoginDialog::LoginDialog(QWidget *parent)
     this->setLayout(&this->ui_.mainLayout);
     this->ui_.mainLayout.addWidget(&this->ui_.tabWidget);
 
+    this->ui_.tabWidget.addTab(&this->ui_.twitchDevice, "Twitch (Device)");
     this->ui_.tabWidget.addTab(&this->ui_.basic, "Basic");
     this->ui_.tabWidget.addTab(&this->ui_.advanced, "Advanced");
+    this->ui_.tabWidget.addTab(&this->ui_.kick, "Kick");
+    this->ui_.tabWidget.setCurrentIndex(0);
 
     this->ui_.buttonBox.setStandardButtons(QDialogButtonBox::Close);
 
@@ -276,9 +279,6 @@ LoginDialog::LoginDialog(QWidget *parent)
                      });
 
     this->ui_.mainLayout.addWidget(&this->ui_.buttonBox);
-
-    this->ui_.tabWidget.addTab(&this->ui_.twitchDevice, "Twitch (Device)");
-    this->ui_.tabWidget.addTab(&this->ui_.kick, "Kick");
 }
 
 }  // namespace chatterino
