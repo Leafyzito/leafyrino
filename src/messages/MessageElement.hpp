688a965f69f163b4cacdb06d578410fb9a902b3b
Jilence <68006348+Jilence@users.noreply.github.com>
2026-09-07 01:48:20 +0200

    fix(badges): unique MessageElementFlag bits for DankChat, FFZ:AP, and Chatsen (#18)
    Co-authored-by: leafy <rafaelzao2003@gmail.com>

--- src/messages/MessageElement.hpp
+++ src/messages/MessageElement.hpp
@@ -97,9 +97,9 @@ enum class MessageElementFlag : int64_t {
     // - FolhinhaBot Supporter
     BadgeFolhinha = (1LL << 62),
 
-    BadgeFfzAp = (1LL << 38),
-    BadgeDankChat = (1LL << 39),
-    BadgeChatsen = (1LL << 41),
+    BadgeFfzAp = (1LL << 43),
+    BadgeDankChat = (1LL << 42),
+    BadgeChatsen = (1LL << 44),
 
     Badges = BadgeGlobalAuthority | BadgePredictions | BadgeChannelAuthority |
              BadgeSubscription | BadgeVanity | BadgeChatterino | BadgeSevenTV |
